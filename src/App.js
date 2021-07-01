7f2f487002972eda502b5fa38548d6056524333e
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-06-30 20:58:00 -0500

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+    <footer>
+      This project was coded by <a href="https://www.linkedin.com/in/rachel-dyer-b1681457/" target="_blank"> Rachel Dyer </a> and is {""}
+    <a href="https://github.com/KholFyre/react-weather-app" target="_blank">
+    open-sourced on Github.
+    
+    </a>
+    </footer>
     </div>
   );
 }
-
