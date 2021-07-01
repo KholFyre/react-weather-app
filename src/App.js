a262d8d834b63e08f5b8a58fee5655e485b3067e
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-06-30 21:02:00 -0500

    Added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ export default function App() {
     <div className="App">
       Weather App
     <footer>
-      This project was coded by <a href="https://www.linkedin.com/in/rachel-dyer-b1681457/" target="_blank"> Rachel Dyer </a> and is {""}
-    <a href="https://github.com/KholFyre/react-weather-app" target="_blank">
+      This project was coded by <a href="https://www.linkedin.com/in/rachel-dyer-b1681457/" rel="noreferrer" target="_blank"> Rachel Dyer </a> and is {""}
+    <a href="https://github.com/KholFyre/react-weather-app" rel="noreferrer" target="_blank">
     open-sourced on Github.
     
     </a>
