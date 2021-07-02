b842fe2e96e7baa9a1892b0ab7b48a60cc34835f
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-07-01 22:31:47 -0500

    Built the basic HTML

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import Weather from "./Weather";
 import './App.css';
 
-export default function App() {
+export default function App () {
   return (
     <div className="App">
-        <div className="container">
-      <h1> Weather App </h1>
+        <div className="Container">
+      <Weather/>
     <footer>
       This project was coded by <a href="https://www.linkedin.com/in/rachel-dyer-b1681457/" rel="noreferrer" target="_blank"> Rachel Dyer </a> and is {""}
     <a href="https://github.com/KholFyre/react-weather-app" rel="noreferrer" target="_blank">
