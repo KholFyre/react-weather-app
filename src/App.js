618a417726984fea372786d48e01cdc6f1b2d613
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-07-01 21:48:28 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+        <div className="container">
+      <h1> Weather App </h1>
     <footer>
       This project was coded by <a href="https://www.linkedin.com/in/rachel-dyer-b1681457/" rel="noreferrer" target="_blank"> Rachel Dyer </a> and is {""}
     <a href="https://github.com/KholFyre/react-weather-app" rel="noreferrer" target="_blank">
@@ -13,6 +14,7 @@ export default function App() {
     
     </a>
     </footer>
+    </div>
     </div>
   );
 }
