96a2391462e103652bfc3856e7747a7931aedd45
KholFyre <82007311+KholFyre@users.noreply.github.com>
2021-07-08 21:45:38 -0500

    Input the Forecast API Using Longitude and Latitude

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ export default function App () {
     <a href="https://github.com/KholFyre/react-weather-app" rel="noreferrer" target="_blank"> open-sourced on Github </a> 
           and {""}
     <a href="https://upbeat-keller-b82650.netlify.app/" rel="noreferrer" target="_blank"> hosted on Netlify. </a> <br/>
-    Photo by Egor Myznik on <a href="https://unsplash.com/@vonshnauzer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" 
-    rel="noreferrer" target="_blank" >Unsplash </a>
+    Background photo by Egor Myznik on <a href="https://unsplash.com/@vonshnauzer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText" 
+    rel="noreferrer" target="_blank" >Unsplash. </a>
     
     </footer>
     </div>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react"; 
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast(){
+export default function WeatherForecast(props){
+    function handleResponse(response){
+        console.log(response.data);
+    }
+
+    let apiKey = "38026c7484f0ebf2041777347312009c";
+    let longitude =  props.coordinates.lon;
+    let latitude= props.coordinates.lat;
+    let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
 
     return  <div className="WeatherForecast">
         <div className="row">
